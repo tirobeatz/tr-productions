7cb73f99e0a35678ff8beb9e08702f5f8af3f6e2
Timo Romeo <timo@Mac.fritz.box>
2026-01-05 22:18:11 +0100

    Remove Framer Motion from homepage

---
+++ src/app/test/test.js
@@ -0,0 +1,14 @@
+export default function Test() {
+  return (
+    <div style={{ 
+      minHeight: '100vh', 
+      background: '#050505', 
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1>Test Page</h1>
+    </div>
+  )
+}
